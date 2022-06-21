68480f0ffdb2f58dee4593657505e01e67f68207
Dara Prak <dprak107@gmail.com>
2022-06-21 10:05:36 -0700

    Added Player and Tournament models

--- src/main/java/com/daraprak/BirdieGolf/models/Player.java
+++ src/main/java/com/daraprak/BirdieGolf/models/Player.java
@@ -1,4 +1,52 @@
 package com.daraprak.BirdieGolf.models;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "players")
 public class Player {
+
+    @Id
+    String email;
+    @NonNull
+    String firstName;
+    @NonNull
+    String lastName;
+    @NonNull
+    String password;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    @JoinTable(name = "players_tournaments",
+            joinColumns = @JoinColumn(name = "player_email"),
+            inverseJoinColumns = @JoinColumn(name = "tournaments_id"))
+    private Set<Tournament> tournaments = new LinkedHashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return email.equals(player.email) && firstName.equals(player.firstName) && lastName.equals(player.lastName) && password.equals(player.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, firstName, lastName, password);
+    }
+
 }
--- src/main/java/com/daraprak/BirdieGolf/models/Tournament.java
+++ src/main/java/com/daraprak/BirdieGolf/models/Tournament.java
@@ -1,4 +1,52 @@
 package com.daraprak.BirdieGolf.models;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "tournaments")
 public class Tournament {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @NonNull
+    String name;
+    @NonNull
+    String date;
+    @NonNull
+    String time;
+    @NonNull
+    String course;
+
+    @ManyToMany(mappedBy = "tournaments", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    private Set<Player> players = new LinkedHashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tournament that = (Tournament) o;
+        return id == that.id && name.equals(that.name) && date.equals(that.date) && time.equals(that.time) && course.equals(that.course);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, date, time, course);
+    }
+
 }
