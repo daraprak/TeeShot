57db840986123bf53bce421560bde794bc937a22
Dara Prak <dprak107@gmail.com>
2022-06-25 16:50:28 -0700

    Added Admin Controller and made some functionality changes

--- src/main/java/com/daraprak/BirdieGolf/dao/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.daraprak.BirdieGolf.dao;
-
-import com.daraprak.BirdieGolf.models.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerRepository extends JpaRepository<Player, String> {
-}
